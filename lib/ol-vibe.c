1617c48b30ae4ee98f95954a0c9e818a30a508c3
Andrew Pullin <pullin@berkeley.edu>
2014-07-01 17:28:11 -0700

    Starting to implement true sinusoidal vibration driver.

--- lib/ol-vibe.c
+++ lib/ol-vibe.c
@@ -11,17 +11,21 @@
 #include "tih.h"
 //#include "math.h"  //for abs
 #include "timer.h"
+#include "libq.h"
+#include <stdint.h>
 
 #define ABS(my_val) ((my_val) < 0) ? -(my_val) : (my_val)
 
-#define T3FREQ 5000
+#define T3FREQ 2048
 #define MAXPWM 1999 //20khz pwm
 //#define MAXPWM 0xf80 //10khz pwm
 
 //static unsigned int maxDC; // from define, MAXPWM
 static unsigned int olvfreq;
-static int chan1amp;
-static int chan2amp;
+static int chan1amp, chan2amp;
+static int chan1dc, chan2dc;
+static _Q15 chan1arg, chan2arg;
+static _Q15 chan1_delta, chan2_delta;
 
 //static unsigned char running = 0;
 
@@ -29,7 +33,7 @@ static unsigned char olphase = 0; // 0 = in phase, 1 = antiphase
 
 //Private functions
 static void SetupTimer3(void);
-static void SetupOC1(void);
+//static void SetupOC1(void);
 
 void olVibeSetup() {
     //get max DC from PWM or leg ctrl module
@@ -37,7 +41,7 @@ void olVibeSetup() {
     //Set up timer
     SetupTimer3();
     //Set up output compare peripheral
-    SetupOC1();
+    //SetupOC1();
     //Default to 50hz @ 0% drive
     olVibeSetAmplitude(1, 0);
     olVibeSetAmplitude(2, 0);
@@ -47,11 +51,13 @@ void olVibeSetup() {
 }
 
 void olVibeStart(void) {
-    _OC1IE = 1;
+    //_OC1IE = 1;
+    T3CONbits.TON = 1;
 }
 
 void olVibeStop(void) {
-    _OC1IE = 0;
+    //_OC1IE = 0;
+    T3CONbits.TON = 0;
 }
 
 void olVibeSetFrequency(unsigned int freq) {
@@ -69,6 +75,7 @@ void olVibeSetFrequency(unsigned int freq) {
     PR3 = (unsigned int)(38976.49770912 / infreq * 2);
     OC1R = PR3 >> 1;
 
+    TMR3 = 0;
     T3CONbits.TON = 1; //start timer
 }
 
@@ -121,11 +128,12 @@ void SetupTimer3() {
     unsigned int T3CON1value, T3PERvalue;
     T3CON1value = T3_ON & T3_SOURCE_INT & T3_PS_1_256 & T3_GATE_OFF &
             T1_SYNC_EXT_OFF & T3_INT_PRIOR_2;
-    T3PERvalue = 10000; //should be ~15hz, or 7.5?
+    T3PERvalue = UINT16_MAX; //65535
     OpenTimer3(T3CON1value, T3PERvalue);
     _T3IE = 0;
 }
 
+/*
 static void SetupOC1(void) {
     // Initialize Output Compare Module
     OC1CONbits.OCM = 0b000; // Disable Output Compare Module
@@ -137,13 +145,29 @@ static void SetupOC1(void) {
     IFS0bits.OC1IF = 0; // Clear Output Compare 1 Interrupt Flag
     //IEC0bits.OC1IE = 1; // Enable Output Compare 1 interrupt
     OC1CONbits.OCM = 0b011; // Select the Output Compare mode
-}
+}*/
 
-/*
+
 void __attribute__((interrupt, no_auto_psv)) _T3Interrupt(void) {
     chan1amp = -chan1amp; //toggle direction
     tiHSetDC(1, chan1amp);
 
+    chan1arg += chan1_delta;
+    chan2arg += chan2_delta;
+
+
+
+
+    chan1dc = _Q15sinPI(arg1);
+    long temp = (long)(chan1dc) * (long)chan1amp;
+    chan1dc = (int)(temp >> 15);
+
+    chan2dc = _Q15sinPI(arg2);
+    temp = (long)(chan2dc) * (long)chan2amp;
+    chan2dc = (int)(temp >> 15);
+
+    tiHSetDC(1, chan1dc);
+    tiHSetDC(2, chan2dc);
+
     _T3IF = 0;
 }
-*/
