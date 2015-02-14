20a1932dbd80b59480416599cdd2eb447eb60b90
Andrew Pullin <pullin@berkeley.edu>
2015-02-13 21:13:24 -0800

    Working verison of joycontrol_mix.py

--- python/joycontrol_mix.py
+++ python/joycontrol_mix.py
@@ -54,7 +54,7 @@ def main():
     # Send robot a WHO_AM_I command, verify communications
     R1.query(retries = 1)
 
-    motorgains = [22000,0,500,0,0,    22000,0,500,0,0]
+    motorgains = [25000,200,100,0,0,    25000,200,100,0,0]
     #motorgains = [25000,50,0,0,25,    25000,50,0,0,25]
 
     R1.setMotorGains(motorgains, retries = 1)
@@ -102,8 +102,8 @@ def main():
             if (abs(thrustInput) < DEADBAND):
                 thrustInput = 0
             
-            left_throt = int(turnInput * MAXTHROT + MAXTHROT*thrustInput )
-            right_throt = int(-turnInput * MAXTHROT + MAXTHROT*thrustInput )
+            left_throt = int(-turnInput * MAXTHROT + MAXTHROT*thrustInput )
+            right_throt = int(turnInput * MAXTHROT + MAXTHROT*thrustInput )
             
             # positive and negative clipping
             if left_throt > MAXTHROT:
